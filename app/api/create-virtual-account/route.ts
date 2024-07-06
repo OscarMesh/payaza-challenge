68db31b09a23ac77e96ebb0ac55a9ca084e71e22
Oscar Okereke <oscarokereke@Oscars-MacBook-Air.local>
2024-07-06 19:09:11 +0100

    code

--- app/api/create-virtual-account/route.ts
+++ app/api/create-virtual-account/route.ts
@@ -1,7 +1,17 @@
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-export async function POST(req: NextResponse, res: NextResponse) {
+function generateRandomReference(length = 10) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "REF";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+export async function POST(req: NextResponse) {
   try {
     const {
       customer_first_name,
@@ -9,6 +19,19 @@ export async function POST(req: NextResponse, res: NextResponse) {
       customer_email,
       customer_phone,
     } = await req.json();
+
+    if (
+      !customer_first_name ||
+      !customer_last_name ||
+      !customer_email ||
+      !customer_phone
+    ) {
+      return NextResponse.json(
+        { error: "All fields are required" },
+        { status: 500 }
+      );
+    }
+    const payment_reference = generateRandomReference();
 
     let data = JSON.stringify({
       service_type: "Account",
@@ -23,7 +46,7 @@ export async function POST(req: NextResponse, res: NextResponse) {
         customer_phone,
         virtual_account_provider: "Premiumtrust",
         payment_amount: 102,
-        payment_reference: "RCO1322XLINE",
+        payment_reference,
       },
     });
 
@@ -32,7 +55,7 @@ export async function POST(req: NextResponse, res: NextResponse) {
       maxBodyLength: Infinity,
       url: "https://router-live.78financials.com/api/request/secure/payloadhandler",
       headers: {
-        Authorization: `Payaza UFo3OC1QS0xJVkUtMEUzRUVCRjYtQjU4Qi00REQyLTlDNzgtOTlEMUU2MTk0NUY`,
+        Authorization: `Payaza ${process.env.NEXT_PAYAZA_PK!} `,
         "Content-Type": "application/json",
       },
       data: data,
@@ -41,7 +64,7 @@ export async function POST(req: NextResponse, res: NextResponse) {
     const response = await axios.request(config);
     return NextResponse.json(response.data);
   } catch (error) {
-    console.error(error);
+    console.log(error);
     return NextResponse.json(
       { error: "Something went wrong..." },
       { status: 500 }
